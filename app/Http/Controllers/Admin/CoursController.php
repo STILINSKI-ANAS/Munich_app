4e484ffb6aab544cfd864f9a50c407f2ca5c659e
03ABDALLAH <abdellahougoummad.10@gmail.com>
2023-06-19 02:55:34 +0100

    add course controller and migrations

--- app/Http/Controllers/Admin/CoursController.php
+++ app/Http/Controllers/Admin/CoursController.php
@@ -3,9 +3,75 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CourseFormRequest;
+use App\Models\Course;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class CoursController extends Controller
 {
-    //
+    
+    public function index()
+    {
+        return view('admin.Course.index');
+    }
+
+
+    public function create()
+    {
+        return view('admin.Course.create');
+    }
+
+
+    public function store(CourseFormRequest $request)
+    {
+        $validatedData = $request->validated();
+        $course = new Course();
+        $course->name = $validatedData['name'];
+        $course->level = $validatedData['level'];
+        $course->overview = $validatedData['overview'];
+        $course->price = $validatedData['price'];
+        $course->time = $validatedData['time'];
+        if ($request->hasFile('image')){
+            $file = $request->file('image');
+            $ext = $file->getClientOriginalExtension();
+            $filename = time().'-'.$ext;
+            $file->move('uploads/course/', $filename);
+            $course->image = $validatedData['image'];
+        }
+        $course->save();
+        return redirect('/admin/Course')->with('message','Course added successfully');
+    }
+
+
+    public function edit(Course $course)
+    {
+        return view('admin.Course.edit',compact('course'));
+    }
+
+
+    public function update(CourseFormRequest $request, $course)
+    {
+        $course = Course::findOrFail($course);
+        $validatedData = $request->validated();
+        $course->name = $validatedData['name'];
+        $course->level = $validatedData['level'];
+        $course->overview = $validatedData['overview'];
+        $course->price = $validatedData['price'];
+        $course->time = $validatedData['time'];
+        if ($request->hasFile('image')){
+            $path = 'uploads/course/'.$course->image;
+            if (File::exists($path)){
+                File::delete($path);
+            }
+
+            $file = $request->file('image');
+            $ext = $file->getClientOriginalExtension();
+            $filename = time().'-'.$ext;
+            $file->move('uploads/course/', $filename);
+            $course->image = $validatedData['image'];
+        }
+        $course->update();
+        return redirect('/admin/Course')->with('message','Course added successfully');
+    }
 }
