c93c2070a98d04ca2398b8c0b75fec56ca68f7e6
STILINSKI-ANAS <anastnx@gmail.com>
2023-06-27 21:02:46 +0100

    add error handling deleteEtudiant ⚠️

--- app/Http/Livewire/Admin/Etudiant/Index.php
+++ app/Http/Livewire/Admin/Etudiant/Index.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\Admin\Etudiant;
 
 use App\Models\Etudiant;
+use Illuminate\Database\QueryException;
 use Livewire\Component;
 
 class Index extends Component
@@ -11,6 +12,8 @@ class Index extends Component
     public $showSubmitButton = 'hidden';
     public $etudiants = [];
     public $idIns;
+    public $errorMessage;
+
     public function mount()
     {
         $this->etudiants = Etudiant::all();
@@ -24,15 +27,27 @@ class Index extends Component
     {
         $this->idIns = 0;
         $this->showSubmitButton = 'hidden';
+        $this->errorMessage = null;
     }
 
-    public function destroyLanguage()
+    public function destroyEtudiant()
     {
-        $etudiant = Etudiant::find($this->idIns);
-        dd($etudiant);
-        $etudiant->delete();
-        $this->showSubmitButton = 'hidden';
-        $this->mount();
+        try {
+            $etudiant = Etudiant::find($this->idIns);
+//        dd($etudiant);
+            $etudiant->delete();
+            $this->showSubmitButton = 'hidden';
+            $this->mount();
+        }catch (QueryException $exception){
+            $errorCode = $exception->getCode();
+
+            if ($errorCode == 23000) {
+                $this->errorMessage = "Cannot delete the student record due to a foreign key constraint.";
+            } else {
+                $this->errorMessage = "An error occurred while deleting the student record.";
+            }
+        }
+
     }
     public function render()
     {
