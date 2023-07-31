0806f00c7da9e7f0490101572c0021e6654413a8
STILINSKI-ANAS <anastnx@gmail.com>
2023-07-31 22:23:37 +0100

    send email working with official email

---
+++ resources/views/emails/greeting.blade.php
@@ -0,0 +1,11 @@
+<!-- resources/views/emails/greeting.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Greetings</title>
+</head>
+<body>
+<h1>Hello Hakim!</h1>
+<p>This is a greeting email sent from Laravel.</p>
+</body>
+</html>
