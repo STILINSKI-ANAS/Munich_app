bf84c62beec7e97022b1ed0901e1395bda8727fb
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2023-11-04 11:37:46 +0100

    place order confirmation modal

--- resources/views/user/Paiement/index.blade.php
+++ resources/views/user/Paiement/index.blade.php
@@ -31,14 +31,23 @@
 {{--</form>--}}
 {{--</body>--}}
 {{--</html>--}}
-
+    <!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Institut Munich - Paiement</title>
+</head>
+<body>
 @extends('layouts.user')
 
 @section('content')
     <div class="checkout_area bg-color-white rbt-section-gap">
         <div class="container">
             <h1>Paiement</h1>
-            <form method="POST" action="{{ route('payementProcess') }}" enctype="multipart/form-data">
+            <form id="paymentForm" method="POST" action="{{ route('payementProcess') }}" enctype="multipart/form-data">
                 @csrf
                 <div class="row g-5 checkout-form">
                     <div class="col-lg-7">
@@ -94,15 +103,15 @@
 
                                     <h4 class="mt--30">Total <span>{{ $total }}DH</span></h4>
 
-                                    <div class="plceholder-button mt--50">
-                                        <button class="rbt-btn btn-gradient hover-icon-reverse" type="submit">
-                                    <span class="icon-reverse-wrapper">
-                                        <span class="btn-text">Place order</span>
-                                    <span class="btn-icon"><i class="feather-arrow-right"></i></span>
-                                    <span class="btn-icon"><i class="feather-arrow-right"></i></span>
-                                    </span>
-                                        </button>
-                                    </div>
+                                    <button id="placeOrderButton" class="rbt-btn btn-gradient hover-icon-reverse"
+                                            type="button">
+                                        <span class="icon-reverse-wrapper">
+                                            <span class="btn-text">Place order</span>
+                                            <span class="btn-icon"><i class="feather-arrow-right"></i></span>
+                                            <span class="btn-icon"><i class="feather-arrow-right"></i></span>
+                                        </span>
+                                    </button>
+
                                 </div>
                             </div>
                         </div>
@@ -111,4 +120,45 @@
             </form>
         </div>
     </div>
+
+
+    <!-- Confirmation Modal -->
+    <div class="modal fade" id="confirmationModal" aria-hidden="true" aria-labelledby="confirmationModalLabel"
+         tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="confirmationModalLabel">Confirmation</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Êtes-vous sûr de vouloir passer cette commande ?
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                    <button class="btn btn-primary" id="confirmOrderButton">
+                        Confirm Order
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
+<!-- jQuery JS -->
+<script src="{{ asset('assets/js/vendor/jquery.js')}}"></script>
+<script>
+    $(document).ready(function () {
+        // Show the confirmation modal when the "Place order" button is clicked
+        $('#placeOrderButton').click(function () {
+            $('#confirmationModal').modal('show');
+        });
+
+        // Handle the "Confirm Order" button in the confirmation modal
+        $('#confirmOrderButton').click(function () {
+            // Submit the form when the confirmation is confirmed
+            $('#paymentForm').submit();
+        });
+    });
+</script>
+</body>
+</html>
