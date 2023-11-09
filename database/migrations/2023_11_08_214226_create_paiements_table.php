10e8319df11c1cb9ab6bc50a4f14e09beab9ce7b
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2023-11-09 22:18:35 +0100

    Update 2023_11_08_214226_create_paiements_table.php

--- database/migrations/2023_11_08_214226_create_paiements_table.php
+++ database/migrations/2023_11_08_214226_create_paiements_table.php
@@ -12,10 +12,11 @@ return new class extends Migration {
     {
         Schema::create('paiements', function (Blueprint $table) {
             $table->id();
-            $table->string('status');
-            $table->decimal('amount', 8, 2);
+            $table->string('oid')->nullable();
+            $table->string('status')->nullable();
+            $table->decimal('amount', 8, 2)->nullable();
             $table->date('date');
-            $table->unsignedBigInteger('etudiant_id');
+            $table->unsignedBigInteger('etudiant_id')->nullable();
             $table->unsignedBigInteger('test_id')->nullable();
             $table->unsignedBigInteger('course_id')->nullable();
             $table->foreign('etudiant_id')->references('id')->on('etudiants')->onDelete('cascade');
