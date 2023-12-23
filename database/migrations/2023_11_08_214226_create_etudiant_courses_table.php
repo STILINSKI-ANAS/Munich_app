f4f809f8c12d1afa4b76a13e7416138d99b1e221
STILINSKI-ANAS <anastnx@gmail.com>
2023-12-23 23:43:17 +0100

    bug fix

---
+++ database/migrations/2023_11_08_214226_create_etudiant_courses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('etudiant_courses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('etudiant_id');
+            $table->unsignedBigInteger('course_id');
+            $table->unsignedBigInteger('paiement_id')->nullable();
+            $table->foreign('etudiant_id')->references('id')->on('etudiants');
+            $table->foreign('course_id')->references('id')->on('courses');
+            $table->foreign('paiement_id')->references('id')->on('paiements');
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('etudiant_courses');
+    }
+};
---
+++ database/migrations/2023_11_08_214226_create_etudiant_tests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('etudiant_tests', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('etudiant_id');
+            $table->unsignedBigInteger('test_id');
+            $table->unsignedBigInteger('paiement_id')->nullable();
+            $table->foreign('etudiant_id')->references('id')->on('etudiants');
+            $table->foreign('test_id')->references('id')->on('tests');
+            $table->foreign('paiement_id')->references('id')->on('paiements');
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('etudiant_tests');
+    }
+};
