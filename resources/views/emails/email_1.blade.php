f3a6b57bce5e167c5af5da6c95381748b86ec3bb
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2023-12-23 21:29:07 +0100

    send 2 emails

---
+++ resources/views/emails/email_1.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body
+    style="font-family: Arial, sans-serif; line-height: 1.6; color: #333; max-width: 600px; margin: 0 auto; padding: 20px;">
+
+<p>Chers candidats,</p>
+
+<p>Nous espérons que ce message vous trouve en excellente forme.</p>
+
+<p>Nous sommes ravis de vous informer que votre inscription aux examens Telc B1/B2 a été enregistrée avec succès. Nous
+    avons bien reçu les détails que vous avez fournis via le formulaire en ligne, et votre demande est actuellement en
+    cours de traitement.</p>
+
+<p>Après la validation de votre inscription, vous recevrez un message de confirmation comprenant les détails nécessaires
+    pour procéder au paiement des frais d'examen et de Vorbereitung. Ce message vous parviendra dans les plus brefs
+    délais.</p>
+
+<p>Nous tenons à vous remercier pour votre participation et votre intérêt envers les examens Telc B1/B2. Si vous avez
+    des questions ou des préoccupations, n'hésitez pas à nous contacter à <a href="mailto:info@institut-munich.ma">info@institut-munich.ma</a>
+    ou par téléphone au <a href="tel:+212667869273">+212 667 869 273</a>.</p>
+
+<p>Nous vous souhaitons beaucoup de succès dans votre préparation aux examens et nous sommes impatients de vous
+    accompagner tout au long de ce processus.</p>
+
+<p>Merci de votre confiance.</p>
+
+<p>Cordialement</p>
+
+</body>
+
+</html>
