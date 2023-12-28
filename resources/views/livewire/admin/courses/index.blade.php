cc0cb4655784aeedf3455cd364f1b086072446e8
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2023-12-28 20:17:28 +0100

    Update index.blade.php

--- resources/views/livewire/admin/courses/index.blade.php
+++ resources/views/livewire/admin/courses/index.blade.php
@@ -8,11 +8,13 @@
                     </span>
         </a>
     </h4>
-    <div class="rbt-default-modal" style="padding: 0px !important;" {{$showSubmitButton}} tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="rbt-default-modal" style="padding: 0px !important;" {{$showSubmitButton}} tabindex="-1"
+         aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
-                    <button type="button" class="rbt-round-btn" data-bs-dismiss="modal" aria-label="Close" {{$showSubmitButton}} wire:click.prevent="hide_validation">
+                    <button type="button" class="rbt-round-btn" data-bs-dismiss="modal" aria-label="Close"
+                            {{$showSubmitButton}} wire:click.prevent="hide_validation">
                         <i class="feather-x"></i>
                     </button>
                 </div>
@@ -20,15 +22,20 @@
                     <div class="inner rbt-default-form">
                         <div class="row">
                             <div class="col-lg-12">
-                                <h5 class="modal-title mb--20" id="exampleModalLabel">Est-ce que vous voulez supprimer ?</h5>
+                                <h5 class="modal-title mb--20" id="exampleModalLabel">Est-ce que vous voulez supprimer
+                                    ?</h5>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="top-circle-shape"></div>
                 <div class="modal-footer pt--30">
-                    <button type="submit" {{$showSubmitButton}} wire:click.prevent="destroyCourse" class="rbt-btn btn-xs bg-color-danger-opacity radius-round color-danger m-1">Oui</button>
-                    <button type="submit" {{$showSubmitButton}} wire:click.prevent="hide_validation" class="rbt-btn btn-xs bg-color-success-opacity radius-round color-success m-1">Non</button>
+                    <button type="submit" {{$showSubmitButton}} wire:click.prevent="destroyCourse"
+                            class="rbt-btn btn-xs bg-color-danger-opacity radius-round color-danger m-1">Oui
+                    </button>
+                    <button type="submit" {{$showSubmitButton}} wire:click.prevent="hide_validation"
+                            class="rbt-btn btn-xs bg-color-success-opacity radius-round color-success m-1">Non
+                    </button>
 
                 </div>
             </div>
@@ -56,7 +63,7 @@
                     <p class="">{{ $course->level }}</p>
                 </td>
                 <td>
-                    <p class="">{{ $course->overview }}</p>
+                    <p class="">{{ \Illuminate\Support\Str::limit($course->overview, 100) }}</p>
                 </td>
                 <td>
                     <span class="b3">{{ $course->content }}</span>
@@ -69,8 +76,13 @@
                 </td>
                 <td>
                     <div class="rbt-button-group justify-content-start">
-                        <a class="rbt-btn btn-xs bg-primary-opacity radius-round" href="{{ url('admin/Course/'.$course->id.'/edit') }}" title="Edit"><i class="feather-edit pl--0"></i></a>
-                        <a wire:click.prevent="createButton({{ $course->id }})" onclick="toggleButton({{ $course->id }})" class="rbt-btn btn-xs bg-color-danger-opacity radius-round color-danger" href="#"  title="Delete"><i class="feather-trash-2 pl--0"></i></a>
+                        <a class="rbt-btn btn-xs bg-primary-opacity radius-round"
+                           href="{{ url('admin/Course/'.$course->id.'/edit') }}" title="Edit"><i
+                                    class="feather-edit pl--0"></i></a>
+                        <a wire:click.prevent="createButton({{ $course->id }})"
+                           onclick="toggleButton({{ $course->id }})"
+                           class="rbt-btn btn-xs bg-color-danger-opacity radius-round color-danger" href="#"
+                           title="Delete"><i class="feather-trash-2 pl--0"></i></a>
                     </div>
                 </td>
             </tr>
