562c47f8cf883293b30b722bb4129dd8ee54d9a2
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2023-12-31 00:22:26 +0100

    awdiiiiii dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,6 +3,12 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Course;
+use App\Models\Etudiant;
+use App\Models\EtudiantTest;
+use App\Models\paiement;
+use App\Models\Test;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -11,6 +17,58 @@ class DashboardController extends Controller
 
     public function index()
     {
-        return view('admin.dashboard');
+        $totalCourses = Course::count();
+        $totalExams = Test::count();
+
+        // Assuming Etudiant is related to User and enrolled in courses or tests
+        $totalStudents = Etudiant::whereIn('id', function ($query) {
+            $query->select('etudiant_id')->from('etudiant_courses')
+                ->union(EtudiantTest::select('etudiant_id'));
+        })->count();
+
+        $totalUsers = User::count();
+        $totalEarnings = paiement::sum('amount'); // Total earnings
+
+        // Pending earnings are those with status_1 = 'confirmé'
+        $pendingEarnings = paiement::where('status_1', 'confirmé')->sum('amount');
+
+        // Fetch courses and tests without limiting to 6
+        $courses = Course::withCount('etudiants as enrolled_students_count')->get();
+        $exams = Test::withCount('etudiants as enrolled_students_count')->get();
+
+        // Calculate earnings for each course
+        foreach ($courses as $course) {
+            $course->earnings = paiement::whereIn('id', function ($query) use ($course) {
+                $query->select('paiement_id')
+                    ->from('etudiant_courses')
+                    ->where('course_id', $course->id);
+            })->sum('amount');
+        }
+
+        // Calculate earnings for each exam
+        foreach ($exams as $exam) {
+            $exam->earnings = paiement::whereIn('id', function ($query) use ($exam) {
+                $query->select('paiement_id')
+                    ->from('etudiant_tests')
+                    ->where('test_id', $exam->id);
+            })->sum('amount');
+        }
+
+        // Sort courses and exams by earnings and limit to 6
+        $courses = $courses->sortByDesc('earnings')->take(6);
+        $exams = $exams->sortByDesc('earnings')->take(6);
+
+//        dd([
+//            'totalCourses' => $totalCourses,
+//            'totalExams' => $totalExams,
+//            'totalStudents' => $totalStudents,
+//            'totalUsers' => $totalUsers,
+//            'totalEarnings' => $totalEarnings,
+//            'pendingEarnings' => $pendingEarnings,
+//            'courses' => $courses->toArray(),
+//            'exams' => $exams->toArray(),
+//        ]);
+
+        return view('admin.dashboard', compact('totalCourses', 'totalExams', 'totalStudents', 'totalUsers', 'totalEarnings', 'pendingEarnings', 'courses', 'exams'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -16,9 +16,10 @@
                                 <i class="feather-book-open"></i>
                             </div>
                             <div class="content">
-                                <h3 class="counter without-icon color-primary"><span class="odometer" data-count="30">00</span>
+                                <h3 class="counter without-icon color-primary"><span class="odometer"
+                                                                                     data-count="{{$totalCourses}}">00</span>
+                                    <span class="rbt-title-style-2 d-block">TOTAL COURSES</span>
                                 </h3>
-                                <span class="rbt-title-style-2 d-block">TOTAL COURSES</span>
                             </div>
                         </div>
                     </div>
@@ -29,13 +30,14 @@
                 <div class="col-lg-4 col-md-4 col-sm-6 col-12">
                     <div class="rbt-counterup variation-01 rbt-hover-03 rbt-border-dashed bg-secondary-opacity">
                         <div class="inner">
-                            <div class="rbt-round-icon bg-secondary-opacity">
-                                <i class="feather-monitor"></i>
+                            <div class="rbt-round-icon bg-primary-opacity">
+                                <i class="feather-clipboard"></i>
                             </div>
                             <div class="content">
-                                <h3 class="counter without-icon color-secondary"><span class="odometer" data-count="10">00</span>
+                                <h3 class="counter without-icon color-primary"><span class="odometer"
+                                                                                     data-count="{{$totalExams}}">00</span>
+                                    <span class="rbt-title-style-2 d-block">TOTAL EXAMS</span>
                                 </h3>
-                                <span class="rbt-title-style-2 d-block">TOTAL EXAMS</span>
                             </div>
                         </div>
                     </div>
@@ -48,10 +50,11 @@
                     <div class="rbt-counterup variation-01 rbt-hover-03 rbt-border-dashed bg-violet-opacity">
                         <div class="inner">
                             <div class="rbt-round-icon bg-violet-opacity">
-                                <i class="feather-award"></i>
+                                <i class="feather-dollar-sign"></i>
                             </div>
                             <div class="content">
-                                <h3 class="counter without-icon color-violet"><span class="odometer" data-count="7">00</span>
+                                <h3 class="counter without-icon color-violet"><span class="odometer"
+                                                                                    data-count="{{$pendingEarnings}}">00</span>
                                 </h3>
                                 <span class="rbt-title-style-2 d-block">pending Earnings</span>
                             </div>
@@ -68,7 +71,8 @@
                                 <i class="feather-users"></i>
                             </div>
                             <div class="content">
-                                <h3 class="counter without-icon color-pink"><span class="odometer" data-count="160">00</span>
+                                <h3 class="counter without-icon color-pink"><span class="odometer"
+                                                                                  data-count="{{$totalStudents}}">00</span>
                                 </h3>
                                 <span class="rbt-title-style-2 d-block">Total Students</span>
                             </div>
@@ -82,10 +86,11 @@
                     <div class="rbt-counterup variation-01 rbt-hover-03 rbt-border-dashed bg-coral-opacity">
                         <div class="inner">
                             <div class="rbt-round-icon bg-coral-opacity">
-                                <i class="feather-gift"></i>
+                                <i class="feather-user"></i>
                             </div>
                             <div class="content">
-                                <h3 class="counter without-icon color-coral"><span class="odometer" data-count="20">00</span>
+                                <h3 class="counter without-icon color-coral"><span class="odometer"
+                                                                                   data-count="{{$totalUsers}}">00</span>
                                 </h3>
                                 <span class="rbt-title-style-2 d-block">Total users</span>
                             </div>
@@ -102,8 +107,8 @@
                                 <i class="feather-dollar-sign"></i>
                             </div>
                             <div class="content">
-                                <h3 class="counter color-warning"><span class="odometer" data-count="25000">00</span>
-                                </h3>
+                                <h3 class="counter color-warning"><span class="odometer"
+                                                                        data-count="{{$totalEarnings}}">00</span></h3>
                                 <span class="rbt-title-style-2 d-block">Total Earnings</span>
                             </div>
                         </div>
@@ -125,6 +130,7 @@
             </div>
             <div class="row gy-5">
                 <div class="col-lg-12">
+                    <!-- Courses Table -->
                     <div class="rbt-dashboard-table table-responsive">
                         <table class="rbt-table table table-borderless">
                             <thead>
@@ -135,32 +141,19 @@
                             </tr>
                             </thead>
                             <tbody>
-                            <tr>
-                                <th><a href="#">Accounting</a></th>
-                                <td>50</td>
-                                <td>10.000 MAD</td>
-                            </tr>
-                            <tr>
-                                <th><a href="#">Marketing</a></th>
-                                <td>40</td>
-                                <td>10.000 MAD</td>
-                            </tr>
-                            <tr>
-                                <th><a href="#">Web Design</a></th>
-                                <td>75</td>
-                                <td>10.000 MAD</td>
-                            </tr>
-                            <tr>
-                                <th><a href="#">Graphic</a></th>
-                                <td>20</td>
-                                <td>10.000 MAD</td>
-                            </tr>
+                            @foreach($courses as $course)
+                                <tr>
+                                    <th><a href="#">{{ $course->level }}</a></th>
+                                    <td>{{ $course->enrolled_students_count }}</td>
+                                    <td>{{ $course->earnings }} Dh</td>
+                                </tr>
+                            @endforeach
                             </tbody>
                         </table>
                     </div>
-
                     <div class="load-more-btn text-center">
-                        <a class="rbt-btn-link" href="#">Browse All Course<i class="feather-arrow-right"></i></a>
+                        <a class="rbt-btn-link" href="{{ url('/admin/Course') }}">Browse All Courses<i
+                                class="feather-arrow-right"></i></a>
                     </div>
                 </div>
             </div>
@@ -168,56 +161,44 @@
         </div>
     </div>
     <div class="rbt-dashboard-content bg-color-white rbt-shadow-box mb--60">
-    <div class="content">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="section-title">
-                    <h4 class="rbt-title-style-3">My Exams</h4>
+        <div class="content">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-title">
+                        <h4 class="rbt-title-style-3">My Exams</h4>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="row gy-5">
-            <div class="col-lg-12">
-                <div class="rbt-dashboard-table table-responsive">
-                    <table class="rbt-table table table-borderless">
-                        <thead>
-                        <tr>
-                            <th>Exam Name</th>
-                            <th>Enrolled</th>
-                            <th>Earnings</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <th><a href="#">Accounting</a></th>
-                            <td>50</td>
-                            <td>10.000 MAD</td>
-                        </tr>
-                        <tr>
-                            <th><a href="#">Marketing</a></th>
-                            <td>40</td>
-                            <td>10.000 MAD</td>
-                        </tr>
-                        <tr>
-                            <th><a href="#">Web Design</a></th>
-                            <td>75</td>
-                            <td>10.000 MAD</td>
-                        </tr>
-                        <tr>
-                            <th><a href="#">Graphic</a></th>
-                            <td>20</td>
-                            <td>10.000 MAD</td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
-
-                <div class="load-more-btn text-center">
-                    <a class="rbt-btn-link" href="#">Browse All Exams<i class="feather-arrow-right"></i></a>
+            <div class="row gy-5">
+                <div class="col-lg-12">
+                    <!-- Exams Table -->
+                    <div class="rbt-dashboard-table table-responsive">
+                        <table class="rbt-table table table-borderless">
+                            <thead>
+                            <tr>
+                                <th>Exam Name</th>
+                                <th>Enrolled</th>
+                                <th>Earnings</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($exams as $exam)
+                                <tr>
+                                    <th><a href="#">{{ $exam->name }}</a></th>
+                                    <td>{{ $exam->enrolled_students_count }}</td>
+                                    <td>{{ $exam->earnings }} Dh</td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="load-more-btn text-center">
+                        <a class="rbt-btn-link" href="{{ url('/admin/Test') }}">Browse All Tests<i
+                                class="feather-arrow-right"></i></a>
+                    </div>
                 </div>
             </div>
-        </div>
 
+        </div>
     </div>
-</div>
 @endsection
