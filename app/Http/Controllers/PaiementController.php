ddaf457ce71236e18db5d8cec5ec93f4ed66ada4
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2024-01-09 00:53:35 +0100

    Update PaiementController.php

--- app/Http/Controllers/PaiementController.php
+++ app/Http/Controllers/PaiementController.php
@@ -128,7 +128,7 @@ class PaiementController extends Controller
         $test = Test::findOrFail($validatedData['test_id']);
 
         // Proceed with the payment process
-        return $this->savePayment($validatedData, $etudiant, $test, $etudiantTest);
+        return $this->savePayment($validatedData, $etudiant, $test, null, $etudiantTest, null);
     }
 
     /**
@@ -182,7 +182,7 @@ class PaiementController extends Controller
 
         $course = Course::findOrFail($validatedData['course_id']);
 
-        return $this->savePayment($validatedData, $etudiant, null, $course);
+        return $this->savePayment($validatedData, $etudiant, null, $course, null, $etudiantCourse);
     }
 
     /**
@@ -316,7 +316,7 @@ class PaiementController extends Controller
     /**
      * Save New Paiement Method (can be test or course) so dynamic
      */
-    protected function savePayment(array $validatedData, $etudiant, $test, $etudiantTest)
+    protected function savePayment(array $validatedData, $etudiant, $test, $course, $etudiantTest = null, $etudiantCourse = null)
     {
         $date = now();
 
@@ -370,6 +370,7 @@ class PaiementController extends Controller
 
         return redirect()->route('paymentSuccess', ['oid' => $oid, 'languages' => $languages]);
     }
+
     /**
      * Ok response from cmi
      */
