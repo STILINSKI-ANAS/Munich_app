a9bd592382c9794c40b077d461e580846a76a7b0
STILINSKI-ANAS <101132341+STILINSKI-ANAS@users.noreply.github.com>
2024-06-21 21:31:39 +0100

    pass in language test, cat

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Exam;
+use App\Models\Language;
+use App\Models\Test;
 use Illuminate\Http\Request;
 
 class ExamController extends Controller
@@ -10,7 +13,15 @@ class ExamController extends Controller
     //
     public function index()
     {
+        $languages = Language::all();
+        $tests = Test::where('is_hidden', false)->get();
+        $categories = Category::all();
         $exams = Exam::all();
-        return view('user.exams.index', compact('exams'));
+        return view('user.exams.index')->with([
+            'languages' => $languages,
+            'tests' => $tests,
+            'categories' => $categories,
+            'exams' => $exams
+        ]);
     }
 }
